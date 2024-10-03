5ab164f858b4211117c69c29ae7a4b7ae1abbc9b
faby2003 <fathimasalim042@gmail.com>
2024-10-03 20:12:06 +0530

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,226 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 03, 2024 at 04:34 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `watch_store`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brand`
+--
+
+CREATE TABLE `brand` (
+  `brandname` varchar(40) NOT NULL,
+  `brandid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `brand`
+--
+
+INSERT INTO `brand` (`brandname`, `brandid`) VALUES
+('fastrack', 2),
+('casio', 3),
+('Titan', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `cid` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `password` varchar(30) NOT NULL,
+  `usertype` int(4) NOT NULL,
+  `email` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`password`, `usertype`, `email`) VALUES
+('asd', 1, 'thufail9613@gmail.com'),
+('asd', 1, 'thufail9613@gmail.com'),
+('qwerty', 1, 'm@gmail.com'),
+('9613', 1, 'remu@gmail.com'),
+('123', 1, 'mmm3@gmail.com'),
+('3366', 1, 'kunj@gmail.com'),
+('admin123', 0, 'admin@gmail.com'),
+('', 1, ''),
+('123', 1, 'fathima@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `order_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `total_price` decimal(10,2) NOT NULL,
+  `status` varchar(40) NOT NULL,
+  `address` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `user_id`, `product_id`, `quantity`, `order_date`, `total_price`, `status`, `address`) VALUES
+(11, 14, 2, 1, '2024-09-20 09:48:00', 15000.00, 'COD', 'Gokulam(h) near perikapalam aluva-2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `model` varchar(30) NOT NULL,
+  `price` int(10) NOT NULL,
+  `quantity` int(30) NOT NULL,
+  `image` varchar(50) NOT NULL,
+  `productid` int(10) NOT NULL,
+  `pdiscription` varchar(200) NOT NULL,
+  `brand` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`model`, `price`, `quantity`, `image`, `productid`, `pdiscription`, `brand`) VALUES
+('GA 100', 10000, 9, 'gshock.avif', 1, 'From G-SHOCK, the toughness watches that are ever evolving in an endless pursuit of strength, this combination model combines analog and digital displays. Equipped with a millisecond stopwatch and spe', 'casio'),
+('GA 1000', 15000, 48, 'gshock.avif', 2, 'From G-SHOCK, the toughness watches that are ever evolving in an endless pursuit of strength, this combination model combines analog and digital displays. Equipped with a millisecond stopwatch and spe', 'casio');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `Name` varchar(15) NOT NULL,
+  `Number` int(12) NOT NULL,
+  `Email` varchar(40) NOT NULL,
+  `Address` varchar(40) NOT NULL,
+  `Password` varchar(15) NOT NULL,
+  `UserID` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`Name`, `Number`, `Email`, `Address`, `Password`, `UserID`) VALUES
+('midlaj', 2147483647, 'm@gmail.com', 'qwerty', 'qwerty', 15),
+('ramesh', 1234567890, 'remu@gmail.com', 'arrrstd', '9613', 16),
+('fathima', 123456789, 'fathima@gmail.com', 'karo', '123', 20);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `brand`
+--
+ALTER TABLE `brand`
+  ADD PRIMARY KEY (`brandid`);
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`productid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `brand`
+--
+ALTER TABLE `brand`
+  MODIFY `brandid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `productid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `UserID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
